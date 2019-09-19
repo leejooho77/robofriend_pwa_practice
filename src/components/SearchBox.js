fd52be91152915a9907dba72d3cb901c977ed457
Juho Lee <leejh2307@gmail.com>
2019-09-19 19:39:19 -0400

    added favicon icon 192x192 & 512x512, made change in manifest, and fix some performance part

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
-const SearchBox = ({ searchfield, searchChange }) => {
+const SearchBox = ({ searchChange }) => {
   return (
     <div className='pa2'>
       <input
         className='pa3 ba b--green bg-lightest-blue'
+        aria-label='Search Robots'
         type='search'
         placeholder='search robots'
         onChange={searchChange}
